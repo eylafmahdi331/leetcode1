adb7c950955d6e6a062e45f40fd6c8bcfc4d22a6
Eylaf Mahdi Elnour <eylafmahdi@gmail.com>
2026-08-06 13:46:07 +0800

    Implement findTarget for BST to find two sum

---
+++ Two Sum IV - Input is a BST.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def findTarget(self, root, k):
+        seen = set()
+
+        def dfs(node):
+            if not node:
+                return False
+
+            needed = k - node.val
+
+            if needed in seen:
+                return True
+
+            seen.add(node.val)
+
+            return dfs(node.left) or dfs(node.right)
+
+        return dfs(root)
