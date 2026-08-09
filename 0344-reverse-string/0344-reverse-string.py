424041fd47e3c5ecf22dd686ae5183e0e3d57049
Eylaf Mahdi Elnour <eylafmahdi@gmail.com>
2026-08-09 12:15:27 +0800

    Time: 0 ms (100%), Space: 19.8 MB (42.92%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -5,12 +5,5 @@ class Solution(object):
         :rtype: None Do not return anything, modify s in-place instead.
         """
         
-        right = len(s) - 1
-        for left in range (len(s)):
-            if right <= left:
-                break
-
-            s[left], s[right] = s[right], s[left]  
-            right -= 1
-    
+        s.reverse()
         
