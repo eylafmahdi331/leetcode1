f4305e3ecf1d46f2e6beaf484ecbbd0da310e00d
Eylaf Mahdi Elnour <eylafmahdi@gmail.com>
2026-08-10 14:03:40 +0800

    Time: 0 ms (100%), Space: 12.3 MB (88.92%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def rotateString(self, s, goal):
+        """
+        :type s: str
+        :type goal: str
+        :rtype: bool
+        """
+
+   
+        new = []
+        for i in range(len(s)):
+            new.append(s[i])
+        counter = 0
+        while counter < len(new):
+            new.append(new[0])
+            new.remove(new[0])
+            new_str = ''.join(new)
+
+            if new_str == goal:
+                return True
+                break
+            else:
+                if counter == len(new) - 1:
+                    return False
+                    break
+  
+
+ 
+            counter += 1
+        
