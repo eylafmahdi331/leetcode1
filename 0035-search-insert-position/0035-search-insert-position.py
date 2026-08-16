f7b4b32d0e4e35f6ed53528c389befe7a169aabc
Eylaf Mahdi Elnour <eylafmahdi@gmail.com>
2026-08-16 15:37:04 +0800

    Time: 0 ms (100%), Space: 12.9 MB (65.98%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -5,21 +5,13 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        high = len(nums) - 1
-        low = 0
+        
 
         if target not in nums:
             nums.append(target)
             nums.sort()
             return nums.index(target)
+        else:
+            return nums.index(target)
 
-        while low <= high:
-            middle  = (low + high) // 2
-            if nums[middle] == target:
-                return middle
-                break
-            elif nums[middle] < target:
-                low = middle + 1
-            else:
-                high = middle - 1
         
