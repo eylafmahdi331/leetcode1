487aec21bfc654b206c0bb3c5f584bfca8058ef5
Eylaf Mahdi Elnour <eylafmahdi@gmail.com>
2026-08-19 13:53:23 +0800

    Time: 338 ms (5.39%), Space: 33 MB (16.84%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -0,0 +1,52 @@
+class Solution(object):
+    def permuteUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        path = []
+        final = []
+        used = [False] * len(nums)
+
+        def backtrack():
+            if len(path) == len(nums):
+                final.append(path[:])
+                return
+
+            for i in range(len(nums)):
+                if used[i]:
+                    continue
+
+                used[i] = True
+                path.append(nums[i])
+
+                backtrack()
+
+                path.pop()
+                used[i] = False
+
+        backtrack()
+
+        return  [list(x) for x in set(tuple(x) for x in final)]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
