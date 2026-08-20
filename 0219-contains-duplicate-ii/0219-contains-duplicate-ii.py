f2e2519fb1edb8e62f655343672831903a315e59
Eylaf Mahdi Elnour <eylafmahdi@gmail.com>
2026-08-20 14:05:30 +0800

    Time: 252 ms (5.18%), Space: 39.9 MB (5.16%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+
+        
+        '''for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+
+                if (((nums[i] == nums[j]) and (abs(i - (j)) <= k ))):
+                    return True
+          
+   
+        return False'''
+        seen = {}
+
+        for i, num in enumerate(nums):
+            if num not in seen:
+                seen[num] = []
+
+            seen[num].append(i)
+        for i in seen:
+            if len(seen[i])> 1:
+
+
+                for a in range(len(seen[i])):
+                    for b in range(a + 1, len(seen[i])):
+
+                        if abs(seen[i][a] - seen[i][b]) <= k:
+                            return True
+
+        return False
+
+        
