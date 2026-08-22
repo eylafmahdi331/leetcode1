aea6568fd26dbca8e11d5d03b13cf362bc97cd93
Eylaf Mahdi Elnour <eylafmahdi@gmail.com>
2026-08-22 15:07:50 +0800

    Time: 2240 ms (8.51%), Space: 12.3 MB (44.68%) - LeetHub

---
+++ 2928-distribute-candies-among-children-i/2928-distribute-candies-among-children-i.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def distributeCandies(self, n, limit):
+        """
+        :type n: int
+        :type limit: int
+        :rtype: int
+        """
+        count = 0
+        for i in range(limit + 1):
+            for j in range (limit +1):
+                for k in range (limit +1):
+                    if (i or j or k <= limit ) and sum([i, j, k]) == n:
+    
+       
+                        count +=1
+        return count
+
+
+                
+
+
+ 
