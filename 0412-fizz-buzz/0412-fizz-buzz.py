47b60028a67878cf49ba4f298370741b2a38b38d
Eylaf Mahdi Elnour <eylafmahdi@gmail.com>
2026-08-23 14:50:37 +0800

    Time: 3 ms (37.55%), Space: 13.3 MB (77.97%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -6,7 +6,7 @@ class Solution(object):
         """
         output = []
         for i in range (1, n+1):
-            if ((i % 3) == 0) and ((i % 5) == 0):
+            if ((i % 15) == 0):
                 output.append("FizzBuzz")
             elif i % 3 == 0:
                 output.append("Fizz")
