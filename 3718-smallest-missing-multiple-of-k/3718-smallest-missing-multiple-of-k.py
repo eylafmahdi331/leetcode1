d1675cd8a6e5e0cc7ef4ba7cc00a28bc91c959e2
Eylaf Mahdi Elnour <eylafmahdi@gmail.com>
2026-08-25 13:56:54 +0800

    Time: 3 ms (28.74%), Space: 12.4 MB (19.54%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.py
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.py
@@ -5,11 +5,9 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        from collections import Counter
-
-        d = Counter(nums)       
+              
         for i in range(1, 1000):
-            if i % k == 0 and i not in d:
+            if i % k == 0 and i not in nums:
                 return i 
            
                     
