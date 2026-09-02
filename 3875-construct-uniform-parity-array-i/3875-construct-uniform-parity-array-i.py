1daf2dff49cca1d8cb12f8718d644d5461686a4a
Eylaf Mahdi Elnour <eylafmahdi@gmail.com>
2026-09-02 23:24:59 +0800

    Time: 0 ms (100%), Space: 12.3 MB (96.25%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def uniformArray(self, nums1):
+        """
+        :type nums1: List[int]
+        :rtype: bool
+        """
+        ''' nums2= []
+        for i in range(len( nums1)):
+            nums2 = nums1[i]
+        all(i % 2 == 0 for i in nums2)
+        return False'''
+        return True
+
+
+
+        
