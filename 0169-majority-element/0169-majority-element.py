194176ae942322327bb8106d4e48cad5fbb63d13
Eylaf Mahdi Elnour <eylafmahdi@gmail.com>
2026-09-23 12:28:25 +0800

    Time: 20 ms (14.47%), Space: 14.6 MB (6.93%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count = {}
+        for i in range(len(nums)):
+            if nums[i] in count:
+                count[nums[i]] += 1
+            else:
+                count[nums[i]] = 1
+        for key, value in count.items():
+            if value == max(count.values()):
+                return key
